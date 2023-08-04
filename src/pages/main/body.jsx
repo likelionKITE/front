b3fd5d4dd356793a0d316b6a447dd6dd3937b289
박유진 <qkrdbw@naver.com>
2023-08-04 14:22:22 +0900

    오타 수정

--- src/pages/main/body.jsx
+++ src/pages/main/body.jsx
@@ -6,7 +6,7 @@ function Main() {
         <div>
             <h1>main</h1>
             <Link to='/festiDetail'>축제개별</Link>
-            <Link to='/destiDeteil'>여행지개별</Link>
+            <Link to='/destiDetail'>여행지개별</Link>
         </div>
     )
 }
