7d81c54844935465bd7b4eca0c11df1837d45c74
BYOUL JUNG <129057067+JungByoul@users.noreply.github.com>
2023-08-05 21:40:03 +0900

    축제 개별 페이지 틀 생성중
    생성

--- src/pages/festiDetail/body.jsx
+++ src/pages/festiDetail/body.jsx
@@ -1,11 +1,41 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link, useNavigate } from 'react-router-dom';
 
 
 function FestiDetail() {
-    return(
-        <h1>FestiDetail</h1>
-    )
+
+
+    return (
+        <>
+            <div>
+                <h1>FestiDetail: 축제 개별 페이지</h1>
+            </div>
+
+            <div class='fest_info'>
+                <div class='fest_info_img'>
+                    <p>이미지</p>
+                </div>
+
+                <div class='fest_info_hub'>
+                    <p>핵심정보</p>
+                </div>
+
+                <div class='fest_info_sub'>
+                    <p>부가정보</p>
+                </div>
+
+                <div class='fest_info_map'>
+                    <p> 각 개별 축제의 지도 api</p>
+                </div>
+
+            </div>
+
+            <div class='review_info'>
+
+            </div>
+        </>
+
+    );
 }
 
 export default FestiDetail;
---
+++ src/pages/festiDetail/style.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+
+function Fest_info() {
+
+    // const ;
+
+    return (
+        <Fest_info_wrapp>
+
+        </Fest_info_wrapp>
+
+
+    );
+}
+
+const Fest_info_wrapp = styled.div`
+    display: flex;
+    flex-direction: column-reverse;
+`
+
+
+export default  Fest_info;
