47aebbb505d3ec49b8d61832b39f9d53ce57a305
habro <gkgud7@gmail.com>
2023-08-08 15:39:50 +0900

    Feat:여행정보 1차 완성

--- src/pages/travelInfo/body.jsx
+++ src/pages/travelInfo/body.jsx
@@ -1,37 +1,72 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Link, useNavigate } from 'react-router-dom';
-
+import Subway from './Images/Subway.jpg'
+import Kobus from './Images/Kobus.jpg'
+import Korail from './Images/Korail.jpg'
+import Map from './Images/Map.jpg'
+import Taxi from './Images/Taxi.jpg'
+import * as style from "./style"
 
 function TravelInfo() {
-    const Embassyurl="https://www.korean.net/homepage/intro/ambassy_site.do";
-    const subwayurl="https://apps.apple.com/us/app/subway-korea/id325924444";
-    const taxiurl="https://www.kakaocorp.com/page/service/service/KakaoT?lang=en";
-    const mapurl="https://apps.apple.com/us/app/naver-map-navigation/id311867728";
-    const trainurl="https://www.letskorail.com/ebizbf/EbizBfKrPassAbout.do";
-    const busurl="https://www.kobus.co.kr/main.do"
-    return(
-        <div>
-            <h1>Helpful information for your trip</h1>
-            <hr></hr>
-            <div className='Embassy'>
-                <h2>Information about embassies in Korea</h2>
-                <button onClick={()=>{window.open(Embassyurl)}}>Check Embassy</button>
-                <h4>When you click this button, you can find the location and contact information of the embassy in Korea.</h4>
-            </div>
-            <hr></hr>
-            <div className='Transportation'>
-                <h2>Information about transportation in Korea</h2>
-                <li>If you want to move inside one  city, you can use city buses, subways, and taxis.</li>
-                <button onClick={()=>{window.open(subwayurl)}}>Subway Korea: 이미지로 링크 연결할 예정</button>
-                <button onClick={()=>{window.open(taxiurl)}}>Kakao T: 이미지로 링크 연결할 예정</button>
-                <button onClick={()=>{window.open(mapurl)}}>Naver map: 이미지로 링크 연결할 예정</button>
-                <li>If you want to travel between different cities, intercity buses and various trains are available.</li>
-                <button onClick={()=>{window.open(trainurl)}}>Train: 이미지로 링크 연결할 예정</button>
-                <button onClick={()=>{window.open(busurl)}}>Express Train: 이미지로 링크 연결할 예정</button>
-                <button>뭔가 하나 더 추가 예정. 3개씩 맞추려고</button>
-            </div>
-        </div>
-    )
+  const Embassyurl = 'https://www.korean.net/homepage/intro/ambassy_site.do';
+
+  return (
+    <style.Container>
+      <div>
+      <h1>Helpful information for your trip</h1>
+      <hr />
+
+      <style.EmbassySection>
+        <h2>Information about embassies in Korea</h2>
+        <button onClick={() => window.open(Embassyurl)}>Check Embassy</button>
+        <h4>When you click this button, you can find the location and contact information of the embassy in Korea.<br></br>After checking the location and contact information of each embassy, ​​you can receive various assistance.</h4>
+      </style.EmbassySection>
+
+      <hr />
+
+      <style.TransportationSection>
+        <h2>Information about transportation in Korea</h2>
+        <h4>If you want to move inside one city, you can use city buses, subways, and taxis.</h4>
+        <a href="https://apps.apple.com/us/app/subway-korea/id325924444" target="_blank" rel="noopener noreferrer">
+            <style.ImageLink>
+                <style.StyledImage src={Subway} alt="subway korea"/>
+                <h4>subway korea</h4>
+                <h5>find timetable of subway</h5>
+            </style.ImageLink>
+        </a>
+        <a href="https://www.kakaocorp.com/page/service/service/KakaoT?lang=en" target="_blank" rel="noopener noreferrer">
+            <style.ImageLink>
+                <style.StyledImage src={Taxi} alt="Kakao Taxi" />
+                <h4>Kakao Taxi</h4>
+                <h5>Book taxi in advance</h5>
+            </style.ImageLink>
+        </a>
+        <a href="https://apps.apple.com/us/app/naver-map-navigation/id311867728" target="_blank" rel="noopener noreferrer">
+            <style.ImageLink>
+                <style.StyledImage src={Map} alt="Naver map" />
+                <h4>Naver map</h4>
+                <h5>Check location of bus in real time</h5>
+            </style.ImageLink>
+        </a>
+          <h4>If you want to travel between different cities, intercity buses and various trains are available.</h4>
+        <a href="https://www.kobus.co.kr/main.do" target="_blank" rel="noopener noreferrer">
+            <style.ImageLink>
+                <style.StyledImage src={Kobus} alt="intercity bus" />
+                <h4>Intercity bus</h4>
+                <h5>find timetable of intercity bus</h5>
+            </style.ImageLink>
+        </a>
+        <a href="https://www.letskorail.com/ebizbf/EbizBfKrPassAbout.do" target="_blank" rel="noopener noreferrer">
+            <style.ImageLink>
+                <style.StyledImage src={Korail} alt="express train" />
+                <h4>Express train</h4>
+                <h5>Reserve various express trains</h5>
+            </style.ImageLink>
+        </a>
+      </style.TransportationSection>
+      </div>
+    </style.Container>
+  );
 }
 
 export default TravelInfo;
--- src/pages/travelInfo/style.jsx
+++ src/pages/travelInfo/style.jsx
@@ -1,1 +1,93 @@
 import styled from 'styled-components';
+const Container = styled.div`
+  text-align: center;
+  border: 40px;
+
+  h1 {
+    margin-bottom: 20px;
+    font-size: 2rem;
+  }
+
+  hr {
+    margin: 30px;
+    border: none;
+    border-top: 2px solid #ccc;
+  }
+`;
+
+const EmbassySection = styled.div`
+margin: 20px 0;
+
+h2 {
+  margin-bottom: 10px;
+  font-size: 1.5rem;
+}
+
+h4 {
+  margin-top: 10px;
+  color: #555;
+}
+
+button {
+  margin-top: 10px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 8px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+}
+`;
+
+const TransportationSection = styled.div`
+margin: 20px 0;
+
+h2 {
+  margin-bottom: 10px;
+  font-size: 1.5rem;
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  padding-left: 0;
+  text-align: center;
+
+  li {
+    margin-bottom: 10px;
+    line-height: 1.5;
+  }
+}
+
+a {
+  display: inline-block;
+  margin-right: 10px;
+}
+`;
+const ImageLink = styled.a`
+display: inline-block;
+margin-right: 10px;
+overflow: hidden;
+transition: transform 0.3s;
+h5{
+    color: #555;
+}
+
+&:hover {
+  transform: scale(1.05);
+}
+`;
+
+const StyledImage = styled.img`
+  width: 200px;
+  height: auto;
+  border-radius: 40px;
+`;
+export {
+    Container, EmbassySection, TransportationSection, ImageLink, StyledImage
+}
