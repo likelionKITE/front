303e974fc9fd6baecb532253579000e8ff806cdb
habro <gkgud7@gmail.com>
2023-08-11 22:32:24 +0900

    여행지랑 축제페이지 일단 목록 다 보이게

--- src/pages/destination/body.jsx
+++ src/pages/destination/body.jsx
@@ -1,13 +1,49 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Link, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 function Destination() {
+    const [data,setData]=useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=>{
+        axios.get('https://port-0-kite-ac2nlkthnw32.sel4.cloudtype.app/travel/list/')
+        .then(response=>{
+            setData(response.data.results);
+            setLoading(false);
+        })
+        .catch(error=>{
+            setError(error);
+            setLoading(false);
+        });
+    },[]);
+    if (loading) {
+        return <p>Loading...</p>;
+      }
+    
+    if (error) {
+        return <p>Error: {error.message}</p>;
+    }
+    
+
+
+    
     return(
         <div>
             <h1>Destination</h1>
+            <ul>
+        {data.map(item => (
+          <li key={item.content_id}>
+            <h2>{item.title}</h2>
+            <img src={item.first_image} alt={item.title} />
+          </li>
+        ))}
+      </ul>
         </div>
-    )
+    );
 }
 
 export default Destination;
--- src/pages/festival/body.jsx
+++ src/pages/festival/body.jsx
@@ -1,11 +1,39 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Link, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
+function FestivalList() {
+  const [festivalList, setFestivalList] = useState([]);
 
-function Festival() {
-    return(
-        <h1>Festival</h1>
-    )
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('https://port-0-kite-ac2nlkthnw32.sel4.cloudtype.app/festival/list/');
+        setFestivalList(response.data.listview_response);
+      } catch (error) {
+        console.error('Error fetching festival list:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Festival List</h1>
+      <ul>
+        {festivalList.map(festival => (
+          <li key={festival.content_id}>
+            <img src={festival.first_image2} alt={festival.title} />
+            <h2>{festival.title}</h2>
+            <p>Start Date: {festival.event_start_date}</p>
+            <p>End Date: {festival.event_end_date}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Festival;
+export default FestivalList;
