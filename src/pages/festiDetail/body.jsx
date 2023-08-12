2c1d42dbd8fead7cd13a4235d36be656ac0cd739
BYOUL JUNG <129057067+JungByoul@users.noreply.github.com>
2023-08-12 19:43:49 +0900

    여행지/축제 개별페이지

--- src/pages/festiDetail/body.jsx
+++ src/pages/festiDetail/body.jsx
@@ -1,80 +1,180 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link, useNavigate } from 'react-router-dom';
-import React, { useEffect } from 'react';
-import {Festi_info} from './style';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Festi_info } from './style';
+import { detailApi } from './apis';
+
+import axios from 'axios';
+
+// const API_KEY = 'https://port-0-kite-ac2nlkthnw32.sel4.cloudtype.app/travel/detail/content_id'; // 실제 API 키로 대체
+// const API_ENDPOINT = `https://port-0-kite-ac2nlkthnw32.sel4.cloudtype.app/travel/detail/2657490`;
 
 function FestiDetail() {
+  // 찜 버튼
+  const [liked, setLiked] = useState(false);
+  const handleLikeClick = () => {
+    setLiked(!liked);
+  }
+  // 정보 불러오기
+  const [festidata, setFestiData] = useState([]);
+  const [detailCommon, setDetailCommon] = useState([]);
 
+  const contentId = useParams().contentId;
 
-    return (
-        <>
-        <Festi_info>
-                <h1>Festival Info</h1>
+  const getDetail = async () => {
+    const nowDetail = await detailApi(contentId);
+    setFestiData(nowDetail[0]);
+    // setDetailCommon(nowDetail[1]);
+  }
+  useEffect(() => {
+    getDetail();
+  }, []);
 
-            <div class='fest_info'>
-                <div class='fest_info_img'>
-                    {/* festiDetail 폴더에 있는 hanriver 이미지 추가 */}
-                    <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width='300px' height='300px'></img>
+  return (
+    <>
+      <Festi_info>
+        <h1>Festival Info(여행지 api 불러옴. 제목이랑 연결 바꿔야함)</h1>
 
-                </div>
+        <div class='fest_info'>
+          <div class='fest_info_top'>
+            <div>
+              <ul>
+                {festidata.map((festi) => (
+                  <li key={festi.content_id}>
+                    <img
+                      class='fest_info_img' src={festi.first_image}>
+                    </img>
+                  </li>
 
-                <div class='fest_info_hub'>
-                    <p>Name</p>
-                    <p><br></br></p>
-                    <p>Address</p>
-                    <p><br></br></p>
-                    <p>My Likes</p>
-                </div>
+                ))}
+              </ul>
+            </div>
 
-                <div class='fest_info_sub'>
-                    <p class='each'>hompage</p>
-                    <p class='each'>play time</p>
-                    <p class='each'>program</p>
-                    <p class='each'>age</p>
-                    <p class='each'>open</p>
-                    <p class='each'>close</p>
-                </div>
+            <div class='fest_info_hub'>
 
-                <div class='fest_info_map'>
+              <ul>
+                {festidata.map((festi) => (
+                  <li key={festi.content_id}>
+                    <div>
+                      <p class='hub_p_tag'>Name</p>
+                      {festi.title}
+                      <p><br></br></p>
+                    </div>
 
-                    {/* <div id="map" style="width:500px;height:400px;">
+                    <div>
+                      <p class='hub_p_tag'>Address</p>
+                      {festi.addr1}
+                      <p><br></br></p>
+                    </div>
+
+                  </li>
+
+                ))}
+              </ul>
+              <div>
+                <p class='hub_p_tag'>My Likes (Click 🤍)</p>
+                <button class='like_button' onClick={handleLikeClick}>{liked ? '🩷' : '🤍'}</button>
+              </div>
+            </div>
+          </div>
+
+          <div class='fest_info_bottom'>
+            <div class='fest_info_bottom_overview+homepage'>
+              <ul>
+                {festidata.map((festi) => (
+                  <li key={festi.content_id}>
+                    <p class='sub_p_tag'>Overview</p>
+                    <div class='sub_overview'>
+                      {festi.detailCommon[0].overview}
+                    </div>
+
+                    <p class='sub_p_tag'>Homepage</p>
+                    <div class='sub_homepage'>
+                      {festi.detailCommon[0].homepage}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div class='fest_info_bottom_else'>
+              <ul>
+                {festidata.map((festi) => (
+                  <li key={festi.content_id}>
+                    <div class='fest_info_bottom_else_details'>
+
+                    <div class='sub_address'>
+                    <p class='sub_p_tag'>Address</p>
+                      {festi.addr2}
+                    </div>
+
+                    
+                    <div class='sub_tel'>
+                    <p class='sub_p_tag'>Tel</p>
+                      {festi.tel} </div>
+
+                    
+                    <div class='sub_age'>
+                    <p class='sub_p_tag'>Age</p>
+                      {festi.detail_intro_travel[0].exp_age_range} </div>
+
+                    
+                    <div class='sub_rest_date'>
+                    <p class='sub_p_tag'>Rest Date</p>
+                      {festi.detail_intro_travel[0].rest_date} </div>
+
+                    <div class='sub_info_center'>
+                    <p class='sub_p_tag'>Info Center</p>
+                      {festi.detail_intro_travel[0].info_center} </div>
+                      </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div class='fest_info_map'>
+
+            {/* <div id="map" style="width:500px;height:400px;">
                     </div> */}
-                    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=b7084bcd5a16ac3795108efeb87312ef"></script>
-                    <MapComponent />
-                    
+            <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=b7084bcd5a16ac3795108efeb87312ef"></script>
+            <MapComponent />
 
-                </div>
 
-            </div>
+          </div>
 
-            <div class='review_info'>
-                <h1>Review</h1>
+        </div>
 
-                <div class='review_posting'>
-                    <p>Title</p>
-                    <input type='text'></input>
-                    
-                    
-                </div>
 
-                <div class='review_posting_detail'>
-                    <p>Detail</p>
-                    <input type='text'></input>
-                    <button>post</button>
-                </div>
+        <div class='review_info'>
+          <h1>Review</h1>
 
-                <div class='review_posted'>
-                    <p>작성된 후기 나열칸</p>
-                    <input type='text'></input>
-                    
-                </div>
+          <div class='review_posting'>
+            <p>Title</p>
+            <input type='text'></input>
 
-            </div>
 
-            </Festi_info>
-        </>
+          </div>
 
-    );
+          <div class='review_posting_detail'>
+            <p>Detail</p>
+            <input type='text'></input>
+            <button>post</button>
+          </div>
+
+          <div class='review_posted'>
+            <p>작성된 후기 나열칸</p>
+            <input type='text'></input>
+
+          </div>
+
+        </div>
+
+      </Festi_info >
+    </>
+
+  );
 }
 
 
@@ -99,7 +199,7 @@ const MapComponent = () => {
     }
   }, []);
 
-  return <div id="map" style={{ width: '100%', height: '400px' }}></div>;
+  return <div id="map" style={{ width: '100%', height: '20px' }}></div>;
 };
 
 
--- src/pages/festiDetail/style.jsx
+++ src/pages/festiDetail/style.jsx
@@ -2,28 +2,70 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 export const Festi_info = styled.div`
+    /* .fest_info{
+        text-align: center;
+    } */
     h1{
         margin-bottom: 30px;
+        margin-left: 50px;
     }
-    .fest_info{
+    //첫번째 박스
+    .fest_info_top{
+        text-align: center;
         display: flex;
         flex-direction: row;
         justify-content: space-evenly;
+        /* margin-left: 50px;
+        margin-right: 50px; */
         
+    }
+    .fest_info_img{
+        width: 450px;
+        height: 350px;
+        /* max-width: 90%;
+        max-height: 90%; */
+
     }
     .fest_info_hub{
         margin-top: 50px;
         margin-left: 50px;
-        margin-right: 50px;
+        margin-right: 20px;
 
-        font-size: 35px;
+        font-size: 30px;
+        font-weight: bold;
     }
-    .fest_info_sub{
-        margin-top: 50px;
-        margin-left: 50px;
-        margin-right: 50px;
-
+    .hub_p_tag{
         font-size: 20px;
+    }
+    .like_button{
+        font-size: 30px;
+        border: none;
+        background-color: white;
+
+    }
+    //두번째 박스
+    .fest_info_bottom{
+        margin: 50px;  
+
+    }
+    .fest_info_bottom_else{
+        margin: 30px;   
+        font-size: 15px;
+
+
+    }
+    .fest_info_bottom_else_details{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+    }
+    .sub_p_tag{
+        text-align: center;
+    }
+    .sub_over_view{
+        margin-left: 100px;
+        margin-right: 100px;
+        display: none
     }
     .each{
         margin-top: 10px;
@@ -33,6 +75,7 @@ export const Festi_info = styled.div`
     .review_info{
         margin-top: 50px;
     }
+    
     /* .review_posting{
         display: flex;
         justify-content: center ;
