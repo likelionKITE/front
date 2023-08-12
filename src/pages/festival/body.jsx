6a9264e9e75763cec29b0f481f33e5db910803da
habro <gkgud7@gmail.com>
2023-08-12 21:22:17 +0900

    Feat:축제페이지 셀렉박스 모두구현

--- src/pages/festival/body.jsx
+++ src/pages/festival/body.jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 function FestivalList() {
   const [festivalList, setFestivalList] = useState([]);
   const [selectedMonth, setSelectedMonth] = useState('01');
+  const [selectedArea, setSelectedArea] = useState('');
   const month_dict = {
     "1": "01",
     "2": "02",
@@ -20,24 +21,53 @@ function FestivalList() {
     "11": "11",
     "12": "12",
   };
+  const area_dict = {
+    "Seoul": "1", "Incheon": "2", "Daejeon": "3", "Daegu": "4", "Gwangju": "5", "Busan": "6", 
+    "Ulsan": "7", "Sejong": "8", "Gyeonggi-do": "31", "Gangwon-do": "32", "Chungcheongbuk-do": "33", 
+    "Chungcheongnam-do": "34", "Gyeongsangbuk-do": "35", "Gyeongsangnam-do": "36", "Jeollabuk-do": "37", "Jeollanam-do": "38", "Jeju-do": "39"
+  };
+  const fetchData = async () => {
+    try {
+      let url = `https://port-0-kite-ac2nlkthnw32.sel4.cloudtype.app/festival/search/?`;
+
+      if (selectedMonth) {
+        url += `month=${selectedMonth}`;
+      }
+
+      if (selectedArea) {
+        if (url.includes('month=')) {
+          url += `&area_code=${selectedArea}`;
+        } else {
+          url += `area_code=${selectedArea}`;
+        }
+      }
+
+      const response = await axios.get(url);
+      setFestivalList(response.data);
+    } catch (error) {
+      console.error('Error fetching festival list:', error);
+    }
+  };
+
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get(`https://port-0-kite-ac2nlkthnw32.sel4.cloudtype.app/festival/search/?month=${selectedMonth}`);
-        setFestivalList(response.data);
-      } catch (error) {
-        console.error('Error fetching festival list:', error);
-      }
-    }
-
     fetchData();
-  }, [selectedMonth]);
+  }, [selectedMonth, selectedArea]);
 
   const handleMonthChange = (event) => {
     const selectedValue = event.target.value;
     setSelectedMonth(selectedValue);
   };
+
+  const handleAreaChange = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedArea(selectedValue);
+  };
+
+  const handleSearch = () => {
+    fetchData();
+  };
+
 
 
   return (
@@ -50,6 +80,15 @@ function FestivalList() {
           </option>
         ))}
       </select>
+      <select onChange={handleAreaChange} value={selectedArea}>
+        <option value="">Select Area</option>
+        {Object.entries(area_dict).map(([key, value]) => (
+          <option key={value} value={value}>
+            {key}
+          </option>
+        ))}
+      </select>
+      <button onClick={handleSearch}>Search</button>
       <ul>
         {festivalList.map(festival => (
           <li key={festival.content_id}>
@@ -59,7 +98,7 @@ function FestivalList() {
         ))}
       </ul>
     </div>
-);
+  );
 }
 
 export default FestivalList;
