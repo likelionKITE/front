2105ef5ccadb426b43d1effcfe86b66fad30f16f
habro <gkgud7@gmail.com>
2023-08-13 18:08:47 +0900

    축제페이지 이번달축제들,셀렉박스들 구현

--- src/pages/festival/body.jsx
+++ src/pages/festival/body.jsx
@@ -2,24 +2,21 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Link, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { StyledFestivalList, StyledPageContainer } from './style';
+function getCurrentMonth() {
+  const currentDate = new Date();
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+  return month;
+}
 
 function FestivalList() {
   const [festivalList, setFestivalList] = useState([]);
-  const [selectedMonth, setSelectedMonth] = useState('01');
+  const [selectedMonth, setSelectedMonth] = useState('');
   const [selectedArea, setSelectedArea] = useState('');
-  const month_dict = {
-    "1": "01",
-    "2": "02",
-    "3": "03",
-    "4": "04",
-    "5": "05",
-    "6": "06",
-    "7": "07",
-    "8": "08",
-    "9": "09",
-    "10": "10",
-    "11": "11",
-    "12": "12",
+  const [currentMonthFestivals, setCurrentMonthFestivals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const month_dict = {"1": "01","2": "02","3": "03","4": "04","5": "05","6": "06",
+  "7": "07","8": "08","9": "09","10": "10","11": "11","12": "12",
   };
   const area_dict = {
     "Seoul": "1", "Incheon": "2", "Daejeon": "3", "Daegu": "4", "Gwangju": "5", "Busan": "6", 
@@ -48,10 +45,21 @@ function FestivalList() {
       console.error('Error fetching festival list:', error);
     }
   };
-
+  const fetchCurrentMonthFestivals = async () => {
+    try {
+      const currentMonth = getCurrentMonth();
+      const url = `https://port-0-kite-ac2nlkthnw32.sel4.cloudtype.app/festival/list/`;
+      const response = await axios.get(url);
+      setCurrentMonthFestivals(response.data.nowview_response);
+      setIsLoading(false); // Set loading state to false
+    } catch (error) {
+      console.error('Error fetching current month festivals:', error);
+    }
+  };
 
   useEffect(() => {
     fetchData();
+    fetchCurrentMonthFestivals();
   }, [selectedMonth, selectedArea]);
 
   const handleMonthChange = (event) => {
@@ -64,16 +72,28 @@ function FestivalList() {
     setSelectedArea(selectedValue);
   };
 
-  const handleSearch = () => {
-    fetchData();
-  };
-
-
-
   return (
     <div>
-      <h1>Festival List</h1>
+      <StyledPageContainer>
+      <StyledFestivalList>
+       <h1>Festival List</h1>
+       <h2>Festival in this month</h2>
+       {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className='this_month'>
+          {currentMonthFestivals.map(festival => (
+            <li key={festival.content_id}>
+              <img src={festival.first_image2} alt={festival.title} />
+              <h2>{festival.title}</h2>
+            </li>
+          ))}
+        </ul>
+      )}
+       
+    <h2>Search your favorite Festival!</h2>
       <select onChange={handleMonthChange} value={selectedMonth}>
+        <option value="">Select Month</option>
         {Object.entries(month_dict).map(([key, value]) => (
           <option key={key} value={value}>
             {key}
@@ -88,7 +108,6 @@ function FestivalList() {
           </option>
         ))}
       </select>
-      <button onClick={handleSearch}>Search</button>
       <ul>
         {festivalList.map(festival => (
           <li key={festival.content_id}>
@@ -97,6 +116,8 @@ function FestivalList() {
           </li>
         ))}
       </ul>
+      </StyledFestivalList>
+    </StyledPageContainer>
     </div>
   );
 }
